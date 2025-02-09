5dc507a3cf791a8d5b773155b56fce31a3a3f470
Lancelin Poulet <p.lancelin09@laposte.net>
2025-02-09 12:21:29 +0100

    Region of interest segmentation

--- Sources/tools.py
+++ Sources/tools.py
@@ -1,9 +1,13 @@
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.patches import Circle
 import pandas as pd
 import os
 from tqdm import tqdm
+from skimage import morphology
+from skimage import img_as_ubyte
+from concurrent.futures import ThreadPoolExecutor
 
 
 def load_images():
@@ -30,3 +34,52 @@ def load_infos():
     infos_df["y_coord"] = pd.to_numeric(infos_df["y_coord"], errors="coerce").fillna(0).astype(int)
 
     return infos_df
+
+
+def remove_label(image):    
+
+    thresh = 20  # Seuil entre 0 et 255, ajustez selon vos besoins
+    binary_image = image.point(lambda p: 255 if p > thresh else 0)
+
+    image = np.array(image)
+    binary_image = np.array(binary_image)
+
+    # Convert to a binary mask (True for 255, False for 0)
+    binary_mask = binary_image > 0
+
+    # Apply erosion and dilation
+    selem1 = morphology.square(100)  # Structuring element, here a 3x3 square
+    eroded_image = morphology.erosion(binary_mask, selem1)
+    selem2 = morphology.disk(70)
+    dilated_image = morphology.dilation(eroded_image, selem2)
+    
+    # Convert the binary results back to an image
+    dilated_image = img_as_ubyte(dilated_image)
+
+    # Convert dilated image to mask (True for dilated, False for everything else)
+    dilated_mask = dilated_image > 0
+
+    # Apply the mask to the original image (use the dilated_mask to select areas)
+    masked_image = np.zeros_like(image)
+    masked_image[dilated_mask] = image[dilated_mask]
+
+    # Convert the result back to an image (if needed)
+    masked_image = img_as_ubyte(masked_image)
+
+    return masked_image
+
+def process_image(image):
+    return remove_label(image)
+
+def get_preprocess_images(breast_images:list=[], save:bool=False) -> list:
+
+    # Parallélisation
+    with ThreadPoolExecutor() as executor:
+        # Map des images avec la fonction `remove_label` tout en préservant l'ordre
+        results = list(tqdm(executor.map(process_image, breast_images), total=len(breast_images)))
+
+    if save :
+        # Conserver les résultats dans un fichier .npy
+        np.save('preprocess_images.npy', results)
+
+    return results
